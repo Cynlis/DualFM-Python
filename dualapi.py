0c08124274b8175d9d2974f527ececd016e2602c
Cynlis <46306494+Cynlis@users.noreply.github.com>
2020-08-11 16:43:52 +0100

    Update dualapi.py

--- dualapi.py
+++ dualapi.py
@@ -1,5 +1,12 @@
 import requests
 
+def version():
+        url = "https://api.dualfm.net/stats"
+        r = requests.get(url)
+        res = r.json()['version']
+        return res
+
+
 class Now:
     def artist():
         url = "https://api.dualfm.net/stats"
@@ -11,21 +18,32 @@ class Now:
         r = requests.get(url)
         res = r.json()['now']['song']
         return res
+    def encoderError():
+        url = "https://api.dualfm.net/stats"
+        r = requests.get(url)
+        res = r.json()['now']['encoderError']
+        return res
 class Listeners:
-    def current(id):
+    def current():
         url = "https://api.dualfm.net/stats"
         r = requests.get(url)
         res = r.json()['listeners']['current']
         return res
-    def peak(id):
+    def peak():
         url = "https://api.dualfm.net/stats"
         r = requests.get(url)
         res = r.json()['listeners']['peak']
         return res
+
 class Presenter:
-    def username(id):
+    def username():
         url = "https://api.dualfm.net/stats"
         r = requests.get(url)
         res = r.json()['presenter']['username']
+        return res
+    def autodj():
+        url = "https://api.dualfm.net/stats"
+        r = requests.get(url)
+        res = r.json()['presenter']['autoDJ']
         return res
 
